8a533001ebff353465e512a1b456c13b3fd20be9
gangoogle <0125gang@gmail.com>
2016-05-14 23:06:29 +0800

    1、增加一个测试方法按钮

--- NaiveWeather/app/src/main/java/com/naive/naiveweather/ui/HomeActivity.java
+++ NaiveWeather/app/src/main/java/com/naive/naiveweather/ui/HomeActivity.java
@@ -2,14 +2,12 @@ package com.naive.naiveweather.ui;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.naive.naiveweather.R;
-
-import net.margaritov.preference.colorpicker.ColorPickerDialog;
+import com.naive.naiveweather.utils.Logger;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,16 +15,29 @@ import butterknife.ButterKnife;
 /**
  * Created by zgyi on 2016/5/12.
  */
-public class HomeActivity extends AppCompatActivity  {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.bt_testinterface)
+    Button mBtTestinterface;
     private Context ctx;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
+        mBtTestinterface.setOnClickListener(this);
 
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_testinterface:
+                Logger.d("YZG", "测试接口");
+                break;
+            default:
+        }
+    }
 }
 
